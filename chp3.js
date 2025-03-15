9b791743f9697ad4507f67b9e86f002d66352482
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-15 13:35:25 +0100

    refactor countB to use countChar

--- chp3.js
+++ chp3.js
@@ -1,14 +1,6 @@
 function countB(str) {
     "use strict";
-
-    let bCount = 0;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === "B")
-            bCount++;
-    }
-
-    return bCount;
+    return countChar(str, "B");
 }
 
 function countChar(str, ch) {
