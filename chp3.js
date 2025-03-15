91b4fae0b746c9640639f636f523d21f8d2b25f9
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-15 13:24:05 +0100

    define countChar

--- chp3.js
+++ chp3.js
@@ -10,3 +10,16 @@ function beanCount(str) {
 
     return bCount;
 }
+
+function countChar(str, ch) {
+    "use strict";
+
+    let charCount = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === ch)
+            charCount++;
+    }
+
+    return charCount;
+}
