16ab8e5814febbef29a2874477e906c1c1dfef1c
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-17 22:17:12 +0100

    first commit - create the road object

---
+++ robot.js
@@ -0,0 +1,24 @@
+/*
+ *
+ *
+ * @Project: A little program that performs various tasks a virtual world
+ *           A mail delivering robot that picks up and drops off parcels.
+ * 
+ * */
+
+// All the roads that connects the 11 places that makes up the virtual town
+const roads = [
+    "Alice's House-Bob's House",
+    "Alice's House-Cabin",
+    "Alice's House-Post Office",
+    "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop",
+    "Marketplace-Farm",
+    "Marketplace-Post Office",
+    "Marketplace-Shop",
+    "Marketplace-Town Hall",
+    "Shop-Town Hall"
+];
+
