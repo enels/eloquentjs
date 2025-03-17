f106a6b0c0f64b20c4bba6eb64b89e0614259f4e
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-17 16:46:57 +0100

    create the Group class

--- chp5_samp.js
+++ chp5_samp.js
@@ -9,7 +9,17 @@ function noisy(f) {
 
 //noisy(Math.min)(3, 2, 1);
 
+function combine (current, element) {
+    return current + element;
+}
+
 function reduce (array, combine, start) {
     let current = start;
-    for (let )
+    for (let element of array) {
+        current = combine (current, element);
+    }
+
+    return current;
 }
+
+console.log(reduce([2, 3, 5, 8, 9, 12, 1], combine, 0));
--- chp6.js
+++ chp6.js
@@ -26,3 +26,62 @@ class Vec {
         return Math.sqrt(this.x**2 + this.y**2);
     }
 }
+
+/*
+ *
+ *
+ * @class Group
+ * @constructor params null
+ * 
+ * 
+ * */
+class Group {
+
+    constructor() {
+        this.group = [];
+    }
+
+    /* add a value to the group(this.group) if it doesn't already exists */
+    add(value) {
+        let groupContainsValue = false;
+
+        this.group.forEach((e) => {if (e == value) groupContainsValue = true});
+
+        if (!groupContainsValue) {
+            this.group.push(value);
+        }
+    }
+
+    /* delete a member from the group array if value is a member */
+    delete(value) {
+        
+        // get the index of the value in the group
+        if (this.has(value))
+            delete this.group[this.group.indexOf(value)];
+    }
+
+    /* check if the argument is a member of the group */
+    has(value) {
+        // get the index of the value in the group
+        let index = this.group.indexOf(value);
+
+        if (index >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /* creates a new Group from an Iterable object (array) */
+    static from(iterObj) {
+        // create new group
+        const newGroup = new Group();
+
+        // add values of iterObj to the new group
+        iterObj.forEach((e) => {newGroup.add(e)});
+
+        return newGroup;
+    }
+}
+
+/* @testing */
