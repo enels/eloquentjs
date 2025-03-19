84170862e65ca87bc168bf96bc71f61b0dacf62f
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-19 19:02:06 +0100

    add the progress indicator for more robust error checking

--- chp8.js
+++ chp8.js
@@ -33,13 +33,17 @@ function getAccount() {
 function transfer(from, amount) {
     if (accounts[from] < amount) return;
 
+    let progress = 0;
     try {
+        accounts[from] -= amount;
+        progress = 1;
         accounts[getAccount()] += amount;
-    }catch(error) {
-        console.log(`An error occured: ${error}`);
+        progress++;
+    }finally {
+        if (progress == 1) {
+            accounts[from] += amount;
+        }
     }
-
-    accounts[from] -= amount;
 }
 
 console.log(transfer("z", 300));
