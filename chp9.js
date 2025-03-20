6f422319514c154e3cf3a9069de69cdca1869f21
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-03-20 16:53:49 +0100

    add basic email, website regex validation

--- chp9.js
+++ chp9.js
@@ -29,11 +29,23 @@ function parseINI(string) {
 }
 
 /* EXERCISES */
-let s = "hstu";
-console.log(/ca(r|t)/.test(s));
-console.log(/p(o|r)op/.test(s));
-console.log(/ferr((et)|y|(ari))/.test(s));
-console.log(/.+(ious)$/.test(s));
-console.log(/\s[\.,:;]/.test(s));
-console.log(/\w{7}/.test(s));
-console.log(/\b[a-df-zA-DF-Z]+\b/.test(s));
+let s = "enomauwaifo4real@gmail.com";
+
+let w = "https://google.com";
+
+// console.log(/ca(r|t)/.test(s));
+// console.log(/p(o|r)op/.test(s));
+// console.log(/ferr((et)|y|(ari))/.test(s));
+// console.log(/.+(ious)$/.test(s));
+// console.log(/\s[\.,:;]/.test(s));
+// console.log(/\w{7}/.test(s));
+// console.log(/\b[a-df-zA-DF-Z]+\b/.test(s));
+
+
+
+// Some extra
+// email validation
+//console.log(/^[0-9a-z]+@\w+\.\w{2,3}$/.test(s));
+
+// website validation
+//console.log(/^(https:\/\/)(www.)?\w+\.\w{2,3}$/.test(w));
